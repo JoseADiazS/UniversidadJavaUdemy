3eff6d074a0ff2a03fff0e00c0fdc4d0233b4998
Jose Armando Diaz Segura <jdiazseg9611@universidadean.edu.co>
2020-06-06 09:50:59 -0500

    Se inicia el proyecto del segundo laboratorio final de el curso Universidad Java de Udemy, se crea el proyecto con sus paquetes y clases, se completa la creacion de clases para el control de excepciones y se crea la clase Pelicula.java

---
+++ LaboratorioFinal2/src/main/java/domain/Pelicula.java
@@ -0,0 +1,52 @@
+package domain;
+
+/**
+ * REpresenta los objetos pelicula utilizados en la aplicacion catalogo de peliculas.
+ */
+public class Pelicula {
+    /**
+     * Atributo nombre de la clase pelicula
+     */
+    private String nombre;
+
+    //Metodos contructores
+
+    /**
+     * Constructor vacio de la clase pelicula
+     */
+    public Pelicula(){}
+
+    /**
+     * Constructor de la clase pelicula.
+     * @param nombre Nombre de la pelicula
+     */
+    public Pelicula(String nombre){
+        this.nombre = nombre;
+    }
+
+    /**
+     * Metodo getter de el atributo Nombre
+     * @return Nombre de la pelicula
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * Metodo setter de el atributo Nombre
+     * @param nombre Nombre de la pelicula
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * Sobrescritura del metodo toString para la clase Pelicula
+     * @return Mensaje: La pelicula es: + Nombre de la pelicula
+     */
+    @Override
+    public String toString() {
+        return "La pelicula es: " + this.getNombre();
+
+    }
+}
